fc44569c4840b4f45165a72061969a95df0e8a3f
Sean Jackson <jacksonsean24@yahoo.co.uk>
2021-05-14 19:55:14 +0100

    Create example.js

---
+++ Basic-Tokenizer/example.js
@@ -0,0 +1,17 @@
+let parse = require('C:/node/lang/parse.js');
+
+let fs = require('fs');
+
+let fileName = process.argv[2];
+
+let config = process.argv[3] + "_parse.json";
+
+let code = fs.readFileSync(fileName,'utf-8');
+
+config = fs.readFileSync(config,'utf-8');
+
+config = JSON.parse(config);
+
+let tokens = parse(code,fileName,config);
+
+console.log(tokens);
